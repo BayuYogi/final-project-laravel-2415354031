bd287b121f4fc0fb76d99b4671a22398b6b532f0
BayuYogi <pitaganakomangbayu@gmail.com>
2026-05-22 16:16:01 +0800

    feature Subscription(migration,model,endpoint)

--- routes/api.php
+++ routes/api.php
@@ -3,23 +3,16 @@
 use App\Http\Controllers\Api\ServiceController; 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CustomerController;
+use App\Http\Controllers\Api\SubscriptionController;
 
 
 Route::apiResource("services", ServiceController::class); 
-Route::patch("services/{service}/activate", [ 
-    ServiceController::class, 
-    "activate", 
-]); 
-Route::patch("services/{service}/deactivate", [ 
-    ServiceController::class, 
-    "deactivate", 
-]);
+Route::patch("services/{service}/activate", [ ServiceController::class, "activate", ]); 
+Route::patch("services/{service}/deactivate", [ ServiceController::class, "deactivate", ]); 
+
 Route::apiResource("customers", CustomerController::class); 
-Route::patch("customers/{customer}/activate", [ 
-    CustomerController::class, 
-    "activate", 
-]); 
-Route::patch("customers/{customer}/deactivate", [ 
-    CustomerController::class, 
-    "deactivate", 
-]);
+Route::patch("customers/{customer}/activate", [CustomerController::class, "activate"]); 
+Route::patch("customers/{customer}/deactivate", [CustomerController::class, "deactivate"]);
+
+Route::apiResource("subscriptions", SubscriptionController::class);
+Route::patch("subscriptions/{subscription}/status", [SubscriptionController::class, "updateStatus"]);
