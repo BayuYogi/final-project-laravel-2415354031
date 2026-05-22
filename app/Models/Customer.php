0d10c5767d57f8ace19d7f2cd046ede7831eb18f
BayuYogi <pitaganakomangbayu@gmail.com>
2026-05-22 15:43:21 +0800

    feature customer(migration,model,endpoint)

---
+++ app/Models/Customer.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Subscription; // <--- WAJIB DITAMBAHKAN
+
+class Customer extends Model
+{
+    protected $fillable = [
+        'customer_id',
+        'name',
+        'email',
+        'phone',
+        'address',
+        'status',
+    ];
+
+    /** 
+     * @return HasMany
+     */ 
+    public function subscriptions(): HasMany 
+    { 
+        return $this->hasMany(Subscription::class); 
+    } 
+}
